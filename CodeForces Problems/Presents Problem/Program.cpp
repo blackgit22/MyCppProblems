1e5ca7b3fc9b5e3ed403266d67066b4524145cb2
RohiTheMori <blackplus1990@gmail.com>
2022-03-20 02:07:19 +0330

    Add files via upload

--- CodeForces Problems/Presents Problem/Program.cpp
+++ CodeForces Problems/Presents Problem/Program.cpp
@@ -2,4 +2,26 @@
 
 int main()
 {
+    int n, counter = 1;
+
+    std::cin >> n;
+
+    int p[n];
+
+    for (int i = 0; i < n; i++)
+    {
+        std::cin >> p[i];
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j <= n; j++)
+        {
+            if (p[j] == counter)
+            {
+                std::cout << j + 1 << " ";
+            }
+        }
+        counter++;
+    }
 }
